082747a568cf7765cb0cdc5bb1c270716ddc38a4
Nick Konstantinov <nick.konstantinov.job@gmail.com>
2023-05-08 19:10:08 +0400

    feat: add popup "modal-info"

--- app.js
+++ app.js
@@ -2,10 +2,8 @@
 
 (async () => {
 
-// General global variables
-const mask = document.querySelector('.mask');
+// General global variable
 const body = document.body;
-const modal = document.querySelector('.modal-info');
 
 // Get book's collection
 let response = await fetch('../books.json');
@@ -100,11 +98,11 @@ function createFrameElem() {
 function createBooksELem(arr) {
     let allBooks = [];
     const shelf = document.querySelector('.catalog__shelf');
-    console.log(shelf);
 
     for (let i = 0; i < arr.length; i++) {
         const bookElem = document.createElement('div');
         bookElem.classList.add('book');
+        bookElem.setAttribute('id', i + 1);
 
         const bookCoverElem = createElem(bookElem, 'div', 'book__cover', '');
         const imgCoverElem = createElem(bookCoverElem, 'img', '', '');
@@ -141,8 +139,129 @@ function createBooksELem(arr) {
     return allBooks;
 }
 
+// Create mask
+function createMaskElem() {
+    const maskElem = createElem(body, 'div', 'mask', '');
+    maskElem.hidden = true;
+}
+
+// Create modal window "modal-info"
+function createModalInfoElem() {
+    const modalInfoElem = createElem(body, 'div', 'modal-info', '');
+    const madalInfoInnerElem = createElem(modalInfoElem, 'div', 'modal-info__inner', '');
+    const modalCrossElem = createElem(modalInfoElem, 'div', 'modal-info__cross', '');
+
+    const madalInfoCoverElem = createElem(madalInfoInnerElem, 'div', 'modal-info__cover', '');
+    const imgModalCoverElem = createElem(madalInfoCoverElem, 'img', '', '');
+
+    const modalInfoInfoElem = createElem(madalInfoInnerElem, 'div', 'modal-info__info', '');
+    const modalInfoAuthorElem = createElem(modalInfoInfoElem, 'h4', 'modal-info__author', '');
+    const modalInfoTitleElem = createElem(modalInfoInfoElem, 'h3', 'modal-info__title', '');
+    const modalInfoPriceElem = createElem(modalInfoInfoElem, 'h4', 'modal-info__price', '');
+    const modalInfoDescriptionElem = createElem(modalInfoInfoElem, 'p', 'modal-info__description', '');
+
+    modalInfoElem.hidden = true;
+}
+
+
 //  ----------  ADD ELEMENTS TO PAGE   ----------
 createFrameElem();
 createBooksELem(booksWithId);
+// Add mask and modal elements
+createMaskElem();
+createModalInfoElem();
+drawModalWindow();
+
+// Popup
+
+// Create new map-books collection
+let mapBooks = new Map();
+
+for (let i = 0; i < booksWithId.length; i++) {
+    mapBooks.set(i + 1, booksWithId[i]);
+}
+
+// Draw "modal-info" window
+function drawModalWindow() {
+    const catalogElem = document.querySelector('.catalog__shelf');
+    catalogElem.addEventListener('click', function(event) {
+        let btnInfo = event.target.closest('.btn-info');
+        let bookElem = event.target.closest('.book');
+
+        if (!btnInfo) return;
+
+        if (!catalogElem.contains(btnInfo)) return;
+
+        maskElem.hidden = !maskElem.hidden;
+        modalInfoElem.hidden = !modalInfoElem.hidden;
+        body.classList.toggle("noscroll");
+        fillModalContent(mapBooks, bookElem.id);
+    });
+}
+
+// Add content to modal window
+function fillModalContent(map, key) {
+    let currentBook = map.get(+key);
+
+    const imgElem = document.querySelector('.modal-info__cover img');
+    imgElem.setAttribute('src', currentBook.imageLink);
+    imgElem.setAttribute('alt', currentBook.author);
+
+    const authorElem = document.querySelector('.modal-info__author');
+    authorElem.innerHTML = currentBook.author;
+
+    const titleElem = document.querySelector('.modal-info__title');
+    titleElem.innerHTML = currentBook.title;
+
+    const priceElem = document.querySelector('.modal-info__price');
+    priceElem.innerHTML = 'Price: &#36;' + currentBook.price;
+
+    const descriptionElem = document.querySelector('.modal-info__description');
+    descriptionElem.innerHTML = currentBook.description;
+}
+
+// Find general modal's and mask's elements
+const maskElem = document.querySelector('.mask');
+const modalInfoElem = document.querySelector('.modal-info');
+const modalCrossElem = document.querySelector('.modal-info__cross');
+
+// Add listener for cross to close modal window
+if (modalCrossElem) {
+    modalCrossElem.addEventListener('click', function() {
+        modalInfoElem.hidden = !modalInfoElem.hidden;
+        maskElem.hidden = !maskElem.hidden;
+        body.classList.remove("noscroll");
+    });
+}
+
+// Add listener for mask when mouseenter event to add hover for cross
+if (maskElem) {
+    maskElem.addEventListener('mouseenter', function() {
+        if (modalCrossElem) {
+            modalCrossElem.classList.toggle('hover');
+        }
+    });
+}
+
+// Add listener for mask when mouseleave event to remove hover for cross
+if (maskElem) {
+    maskElem.addEventListener('mouseleave', function() {
+        if (modalCrossElem) {
+            modalCrossElem.classList.toggle('hover');
+        }
+    });
+}
+
+// Add listener for mask to close modal and hide mask
+if (maskElem) {
+    maskElem.addEventListener('click', function() {
+        if (modalInfoElem) {
+            modalInfoElem.hidden = true;
+            maskElem.hidden = true;
+            body.classList.remove("noscroll");
+        }
+    });
+}
+
 
 })();
